a828cbd2728782ee52b809bd27349bf0348a8d80
maryamhashemi <hashemi.maryam.sadat@gmail.com>
2019-01-24 16:23:35 +0330

    Add SVM and Sudoku_Solver classes & Edit Image_Processing class

---
+++ SVM.py
@@ -0,0 +1,273 @@
+import cv2
+import numpy as np
+import glob
+from Sudoku.Image_Processing import Image_Processing
+
+class SVM:
+    
+    def resize_image(self, src_image, dst_image_height, dst_image_width):
+        src_image_height = src_image.shape[0]
+        src_image_width = src_image.shape[1]
+    
+        if src_image_height > dst_image_height or src_image_width > dst_image_width:
+            height_scale = dst_image_height / src_image_height
+            width_scale = dst_image_width / src_image_width
+            scale = min(height_scale, width_scale)
+            img = cv2.resize(src=src_image, dsize=(0, 0), fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC)
+        else:
+            img = src_image
+    
+        img_height = img.shape[0]
+        img_width = img.shape[1]
+    
+        dst_image = np.ones(shape=[dst_image_height, dst_image_width], dtype=np.uint8)
+        dst_image = np.multiply(dst_image, 255)
+    
+        y_offset = (dst_image_height - img_height) // 2
+        x_offset = (dst_image_width - img_width) // 2
+    
+        dst_image[y_offset:y_offset+img_height, x_offset:x_offset+img_width] = img
+    
+        return dst_image  
+
+    '''
+        Read Train data and labels
+    '''  
+    def read_train_data(self,images_height=32, images_width=32):
+        
+        # Find number of train images 
+        ImagesCount = 0
+        for NumberClass in range(1,10):
+            File_Path = './NumberDataset/Train_Data/' + str(NumberClass) + '/*.jpg'
+            ImagesCount = ImagesCount + len(glob.glob(File_Path))
+            File_Path = './NumberDataset/Train_Data/' + str(NumberClass) + '/*.png'
+            ImagesCount = ImagesCount + len(glob.glob(File_Path))
+        
+        # Train images.    
+        train_images= np.zeros(shape =(ImagesCount,images_height,images_width) ,dtype=np.float32)
+        
+        # Train labels.
+        train_labels= np.zeros(shape = (ImagesCount),dtype=np.int)
+    
+        i=0;
+        
+        for NumberClass in range(1,10):
+            File_Path = './NumberDataset/Train_Data/' + str(NumberClass) + '/*.jpg'
+            jpgimages = ['/'.join(file.split('\\')) for file in glob.glob(File_Path)]
+            
+            File_Path = './NumberDataset/Train_Data/' + str(NumberClass) + '/*.png'
+            pngimages = ['/'.join(file.split('\\')) for file in glob.glob(File_Path)]
+            
+            images =  jpgimages + pngimages   
+             
+            for image in images:
+                # Image reading
+                image = cv2.imread(image , 0)
+                
+                # Image resizing.
+                image = self.resize_image(src_image=image, dst_image_height=images_height, dst_image_width=images_width)
+                
+                # Image normalization.
+                image = image / 255
+                
+                # Image binarization.
+                image = np.where(image >= 0.5, 0, 1)
+                
+                # Image.
+                train_images[i] = image
+    
+                # Label.
+                train_labels[i] = NumberClass
+                
+                i = i+1
+        
+        return train_images,train_labels
+    
+      
+    '''
+        Read Test data and labels
+    '''
+    def read_test_data(self, images_height=32, images_width=32):
+        
+    # Find number of train images 
+        ImagesCount = 0
+        for NumberClass in range(1,10):
+            File_Path = './NumberDataset/Test_Data/' + str(NumberClass) + '/*.jpg'
+            ImagesCount = ImagesCount + len(glob.glob(File_Path))
+            File_Path = './NumberDataset/Test_Data/' + str(NumberClass) + '/*.png'
+            ImagesCount = ImagesCount + len(glob.glob(File_Path))
+        
+        # Train images.    
+        test_images= np.zeros(shape =(ImagesCount,images_height,images_width) ,dtype=np.float32)
+        
+        # Train labels.
+        test_labels= np.zeros(shape = (ImagesCount),dtype=np.int)
+    
+        i=0;
+        
+        for NumberClass in range(1,10):
+            File_Path = './NumberDataset/Test_Data/' + str(NumberClass) + '/*.jpg'
+            jpgimages = ['/'.join(file.split('\\')) for file in glob.glob(File_Path)]
+            
+            File_Path = './NumberDataset/Test_Data/' + str(NumberClass) + '/*.png'
+            pngimages = ['/'.join(file.split('\\')) for file in glob.glob(File_Path)]
+            
+            images =  jpgimages + pngimages   
+             
+            for image in images:
+                # Image reading
+                image = cv2.imread(image , 0)
+                
+                # Image resizing.
+                image = self.resize_image(src_image=image, dst_image_height=images_height, dst_image_width=images_width)
+                
+                # Image normalization.
+                image = image / 255
+                
+                # Image binarization.
+                image = np.where(image >= 0.5, 0, 1)
+                
+                # Image.
+                test_images[i] = image
+    
+                # Label.
+                test_labels[i] = NumberClass
+                
+                i = i+1
+        
+        return test_images,test_labels
+
+    '''
+        Find feature from train data
+    '''    
+    def feature_extractor(self, train_images):
+        winSize = (32,32)
+        blockSize = (14,14)
+        blockStride = (6,6)
+        cellSize = (14,14)
+        nbins = 9
+        derivAperture = 1
+        winSigma = -1.
+        histogramNormType = 0
+        L2HysThreshold = 0.2
+        gammaCorrection = 1
+        nlevels = 64
+        signedGradients = True
+     
+        hog = cv2.HOGDescriptor(winSize,blockSize,
+                                blockStride,cellSize,
+                                nbins,derivAperture,
+                                winSigma,histogramNormType,
+                                L2HysThreshold,gammaCorrection,nlevels,
+                                signedGradients)
+        descriptor = []
+        
+        for im in train_images:
+            
+            im = np.uint8(im)
+            h=hog.compute(im)
+            h = h.flatten()
+            descriptor.append(h)
+            
+        return np.float32(descriptor) 
+
+    '''
+     Train the model
+    '''
+    def training(self,features, train_labels):
+        '''
+            Set up SVM for OpenCV 3
+        '''
+        svm = cv2.ml.SVM_create()
+        
+        '''
+            Set SVM type
+        '''    
+        svm.setType(cv2.ml.SVM_C_SVC)
+        
+        '''
+            Set SVM Kernel to Radial Basis Function (RBF) 
+        '''
+        svm.setKernel(cv2.ml.SVM_RBF)
+        
+        '''
+            Set parameter C
+        '''
+        svm.setC(1)
+        
+        '''
+            Set parameter Gamma
+        '''    
+        svm.setGamma(1)
+        
+        '''
+            Train SVM on training data
+        '''      
+        svm.train(features, cv2.ml.ROW_SAMPLE, train_labels)
+        
+        return svm
+
+    '''
+     Test the model
+    '''
+    def testing(self,model,test_data):
+        testResponse = model.predict(test_data)[1].ravel()
+        return testResponse
+
+    '''
+        Evalute the model
+    '''
+    def evalute(self,test_labels,prediction_label): 
+        
+        confusion_matrix = np.zeros((9,9),dtype = 'int')  
+                
+        for i in range(0,len(prediction_label)): 
+            for j in range(0,9):      
+                if (prediction_label[i] == j + 1):
+                    confusion_matrix[test_labels[i] - 1][j] += 1
+         
+        mean_accuracy =0.0       
+        for i in range(0,9):
+            Sum = 0
+            for j in range(0,9):
+                Sum = Sum + confusion_matrix[i][j]
+                          
+            accuracy = confusion_matrix[i][i] / Sum
+            mean_accuracy = mean_accuracy + accuracy
+        
+        mean_accuracy = mean_accuracy / 9
+         
+        print("confusion matrix = ")
+        print(confusion_matrix)  
+        print("mean accuracy = ", mean_accuracy)
+
+
+def main():
+
+    svm = SVM();
+    print('Reading Train Data ...')
+    train_images, train_labels = svm.read_train_data()
+    
+    print('Reading Test Data ...')
+    test_images, test_labels = svm.read_test_data()
+    
+    print('Extracting hog feature from training samples ...')
+    hog_features = svm.feature_extractor(train_images)
+    
+    print('Training with svm ...')
+    model = svm.training(hog_features, train_labels)
+    
+    print('Extracting hog feature from testing samples ...')
+    hog_features = svm.feature_extractor(test_images)
+    
+    print('Testing ...')
+    prediction_label = svm.testing(model, hog_features)
+    
+    print('Evaluting ...')
+    svm.evalute(test_labels, prediction_label)
+    
+    print(prediction_label)
+
+if __name__ == "__main__":
+    main()
+
---
+++ Sudoko_Solver.py
@@ -0,0 +1,139 @@
+class Sudoko_Solver:
+    
+    def __init__(self):
+        digits   = '123456789'
+        rows     = 'ABCDEFGHI'
+        cols     = digits
+        squares  = self.cross(rows, cols)
+        unitlist = ([self.cross(rows, c) for c in cols] +
+                    [self.cross(r, cols) for r in rows] +
+                    [self.cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')])
+        
+        units = dict((s, [u for u in unitlist if s in u]) 
+                     for s in squares)
+        
+        peers = dict((s, set(sum(units[s],[]))-set([s]))
+                     for s in squares)
+            
+        self.test()
+        
+    '''
+        Cross product of elements in A and elements in B.
+    '''
+    def cross(self,A, B):
+        
+        return [a+b for a in A for b in B]
+
+    def test(self):
+        "A set of unit tests."
+        assert len(self.squares) == 81
+        assert len(self.unitlist) == 27
+        assert all(self.len(self.units[s]) == 3 for s in self.squares)
+        assert all(len(self.peers[s]) == 20 for s in self.squares)
+        assert self.units['C2'] == [['A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2', 'I2'],
+                               ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9'],
+                               ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3']]
+        assert self.peers['C2'] == set(['A2', 'B2', 'D2', 'E2', 'F2', 'G2', 'H2', 'I2',
+                                   'C1', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9',
+                                   'A1', 'A3', 'B1', 'B3'])
+        print ('All tests pass.')
+
+    '''
+        Convert grid to a dict of possible values, {square: digits}, or
+        return False if a contradiction is detected.
+    '''
+    def parse_grid(self,grid):
+        # To start, every square can be any digit; then assign values from the grid.
+        values = dict((s, self.digits) for s in self.squares)
+        for s,d in self.grid_values(grid).items():
+            if d in self.digits and not self.assign(values, s, d):
+                return False ## (Fail if we can't assign d to square s.)
+        return values
+
+    '''
+        Convert grid into a dict of {square: char} with '0' or '.' for empties.
+    '''
+    def grid_values(self, grid): 
+        chars = [c for c in grid if c in self.digits or c in '0.']
+        assert len(chars) == 81
+        return dict(zip(self.squares, chars))
+
+    '''
+        Eliminate all the other values (except d) from values[s] and propagate.
+        Return values, except return False if a contradiction is detected.
+    '''
+    def assign(self, values, s, d):
+        other_values = values[s].replace(d, '')
+        if all(self.eliminate(values, s, d2) for d2 in other_values):
+            return values
+        else:
+            return False
+
+    '''Eliminate d from values[s]; propagate when values or places <= 2.
+        Return values, except return False if a contradiction is detected.'''
+    def eliminate(self, values, s, d):
+        if d not in values[s]:
+            return values ## Already eliminated
+        values[s] = values[s].replace(d,'')
+        ## (1) If a square s is reduced to one value d2, then eliminate d2 from the peers.
+        if len(values[s]) == 0:
+            return False ## Contradiction: removed last value
+        elif len(values[s]) == 1:
+            d2 = values[s]
+            if not all(self.eliminate(values, s2, d2) for s2 in self.peers[s]):
+                return False
+        ## (2) If a unit u is reduced to only one place for a value d, then put it there.
+        for u in self.units[s]:
+            dplaces = [s for s in u if d in values[s]]
+            if len(dplaces) == 0:
+                return False ## Contradiction: no place for this value
+            elif len(dplaces) == 1:
+                # d can only be in one place in unit; assign it there
+                    if not self.assign(values, dplaces[0], d):
+                        return False
+        return values
+
+    '''
+        Display these values as a 2-D grid.
+    '''
+    def display(self,values):
+        width = 1+max(len(values[s]) for s in self.squares)
+        line = '+'.join(['-'*(width*3)]*3)
+        for r in self.rows:
+            print (''.join(values[r+c].center(width)+('|' if c in '36' else '')
+                          for c in self.cols))
+            if r in 'CF': 
+                print (line)
+        print   
+
+    def solve(self,grid): 
+        return self.search(self.parse_grid(grid))
+    
+    '''
+        Using depth-first search and propagation, try all possible values.
+    '''
+    def search(self,values):
+        
+        if values is False:
+            return False ## Failed earlier
+        if all(len(values[s]) == 1 for s in self.squares): 
+            return values ## Solved!
+        ## Chose the unfilled square s with the fewest possibilities
+        n,s = min((len(values[s]), s) for s in self.squares if len(values[s]) > 1)
+        return self.some(self.search(self.assign(values.copy(), s, d)) 
+            for d in values[s])
+
+    '''
+        Return some element of seq that is true.
+    '''
+    def some(self,seq):
+       
+        for e in seq:
+            if e: return e
+        return False   
+
+
+
+
+
+
